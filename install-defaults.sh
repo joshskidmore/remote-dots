7e448a82b3fb8d99be9d8572307d4d9762b1714d
Josh Skidmore <josh@josh.sc>
2017-04-19 06:28:56 -0400

    update to install file

--- install-defaults.sh
+++ install-defaults.sh
@@ -3,11 +3,15 @@
 # check for stow
 command -v stow >/dev/null 2>&1 || { echo >&2 "The GNU utility 'stow' is required, but doesn't exist. Aborting."; exit 1; }
 
+cd dotfiles
+
 echo "Installing bash..."
-stow -t ~ dotfiles/bash
+stow -t ~ bash
 
 echo "Installing tmux..."
-stow -t ~ dotfiles/tmux
+stow -t ~ tmux
 
 echo "Installing vim..."
-stow -t ~ dotfiles/vim
+stow -t ~ vim
+
+cd ..
