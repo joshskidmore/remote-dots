dc490325b4ef9914c16eec2c8ad2232e855f5c4e
Josh Skidmore <josh@josh.sc>
2018-10-12 06:29:49 -0400

    cleanup install-defaults.sh

--- install-defaults.sh
+++ install-defaults.sh
@@ -15,8 +15,6 @@ stow -t ~ bash
 echo "Removing old files..."
 rm ~/.bash_aliases_defaults
 rm ~/.bash_completion_defaults
-rm ~/.bash_completions/*
-rmdir ~/.bash_completions
 
 echo "Installing shared..."
 stow -t ~ shared
