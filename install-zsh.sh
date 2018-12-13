6ffd19b983855471c736673e7bc16b8edd1dfda1
Josh Skidmore <josh@josh.sc>
2018-12-13 06:04:53 -0500

    [zsh] remove zplug / install-zsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -4,8 +4,9 @@ echo "Installing modules..."
 git submodule init
 git submodule update
 
+echo "Clean old zsh..."
+rm -rf ~/.zsh*
+rm -rf ~/.zplug*
+
 echo "Installing zsh..."
 cd dotfiles && stow -t ~ zsh
-
-echo "Installing zplug..."
-zsh -ic "zplug install"
