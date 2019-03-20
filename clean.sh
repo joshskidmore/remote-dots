b2e844774d4ce41e584b75bf2f914c757c4362aa
Josh Skidmore <josh@josh.sc>
2019-03-20 06:02:08 -0400

    added clean.sh

---
+++ clean.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+[[ -h "~/bin" ]] && \
+  echo "Removing ~/bin symlink ..." && \
+  rm ~/bin
+
+[[ -h "~/.config" ]] && \
+  echo "Removing ~/.config symlink ..." && \
+  rm ~/.config
+
+[[ -d "~/vim" ]] && \
+  echo "Removing ~/vim ..." && \
+  rm -rf ~/vim
+
+echo "Clearing ~/.vim/tmp/* ..."
+rm -rf ~/.vim/tmp/*
+
+[[ -h "~/.vim" ]] && \
+  echo "Removing ~/.vim symlink ..." && \
+  rm ~/.vim
+
+echo "Installing defaults..."
+./install-defaults.sh
